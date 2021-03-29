4ec79c9f820eeb3b830e91e6fc0ca44e8bb65544
Colin Wilson <github@colinwilson.dev>
2021-03-29 19:50:17 -0400

    Spells Shoot To The Right

---
+++ Kirks-Adventure/Assets/Scripts/Spell.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spell : MonoBehaviour
+{
+    public GameObject kirk;
+    public GameObject prefabProjectile;
+    private GameObject projectile;
+    public float velocityMult = 4f;
+    private bool isClicking = false;
+    private int kirkMovingRight = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (kirk.GetComponent<Rigidbody>().velocity.x > 0) {
+            kirkMovingRight = 1;
+        } else if (kirk.GetComponent<Rigidbody>().velocity.x < 0) {
+            kirkMovingRight = -1;
+        }
+
+        if (Input.GetMouseButtonDown(0) && !isClicking) {
+            projectile = Instantiate(prefabProjectile) as GameObject;
+            // Start it at the launchPoint
+            projectile.transform.position = kirk.transform.position;// + new Vector3(0.0f, 1f, 0.0f);
+            // projectile.GetComponent<Rigidbody>().isKinematic = true;
+            projectile.GetComponent<Rigidbody>().velocity = new Vector3(40 * kirkMovingRight, 0.0f, 0.0f);
+        }
+
+        if (Input.GetMouseButtonUp(0)) {
+            isClicking = false;
+        }
+    }
+}
