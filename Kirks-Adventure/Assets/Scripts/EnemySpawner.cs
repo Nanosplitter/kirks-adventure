11d454997777d88973b0788ea039c8a41892d35f
Keegan Dwyer <kmd8132000@gmail.com>
2021-03-29 21:22:53 -0400

    Better Enemy Spawner

--- Kirks-Adventure/Assets/Scripts/EnemySpawner.cs
+++ Kirks-Adventure/Assets/Scripts/EnemySpawner.cs
@@ -11,40 +11,33 @@ public class EnemySpawner : MonoBehaviour
     public Vector3 enemyPosMin; // relative to player
     public Vector3 enemyPosMax; // relative to player
 
+    private bool isTripped = false;
 
     public GameObject[] enemyInstances;
 
-    void Awake()
+    void Update()
     {
         enemyInstances = new GameObject[numEnemies];
         GameObject anchor = GameObject.Find("EnemySpawner");
         GameObject enemy;
 
-        for (int i = 0; i < numEnemies; i++)
-        {
-            int prefabNum = Random.Range(0, enemyPrefabs.Length);
-
-            enemy = Instantiate(enemyPrefabs[prefabNum]) as GameObject;
-
-            Vector3 enemyPos = Vector3.zero;
-            enemyPos.x = player.transform.position.x + 15;
-            enemyPos.y = -1;
-
-            enemy.transform.parent = anchor.transform;
-
-            enemyInstances[i] = enemy;
-        }
-    }
-    void Update()
-    {
-        // Iterate over each cloud that was created
-        foreach (GameObject enemy in enemyInstances)
-        {
-            if (player.transform.position.x >= 5)
+        if (player.transform.position.x >= -5 && isTripped == false)
+        { 
+            for (int i = 0; i < numEnemies; i++)
             {
-                print("player transform position x >= 5!!!");
-            }
+                int prefabNum = Random.Range(0, enemyPrefabs.Length);
 
+                enemy = Instantiate(enemyPrefabs[prefabNum]) as GameObject;
+
+                Vector3 enemyPos = new Vector3(player.transform.position.x + 15, player.transform.position.y, 0f);
+
+                enemy.transform.parent = anchor.transform;
+
+                enemyInstances[i] = enemy;
+
+                print("Spawning Enemy!!!");
+            }
+            isTripped = true;
         }
     }
 }
