69fd9a51ec1008809caee18c5101f0cc89c3c33d
Keegan Dwyer <kmd8132000@gmail.com>
2021-04-18 18:09:20 -0400

    Created and Assigned Player Movement Animations
    Kirk walks!

--- Kirks-Adventure/Assets/Scripts/PlayerController.cs
+++ Kirks-Adventure/Assets/Scripts/PlayerController.cs
@@ -7,9 +7,12 @@ public class PlayerController:MonoBehaviour
 {
     CharacterController characterController;
 
+    public Animator animator;
+
     public float speed = 6.0f;
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
+    private bool isClicking = false;
 
     private Vector3 moveDirection = Vector3.zero;
 
@@ -20,6 +23,8 @@ public class PlayerController:MonoBehaviour
 
     void Update()
     {
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+
         if (characterController.isGrounded)
         {
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
--- Kirks-Adventure/Assets/Scripts/Spell.cs
+++ Kirks-Adventure/Assets/Scripts/Spell.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Spell : MonoBehaviour
 {
+    //public Animator animator;
+
     public GameObject kirk;
     public GameObject firePrefab;
     public GameObject waterPrefab;
@@ -39,6 +41,9 @@ public class Spell : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
+
+        //animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+
         // print(Input.GetAxis("Horizontal"));
         if (Input.GetAxis("Horizontal") > 0) {
             kirkMovingRight = 1;
@@ -49,6 +54,7 @@ public class Spell : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && !isClicking) {
             projectile = Instantiate(prefabProjectile) as GameObject;
             // Start it at the launchPoint
+            //animator.SetBool("isShooting", true);
             projectile.transform.position = kirk.transform.position;// + new Vector3(0.0f, 1f, 0.0f);
             // projectile.GetComponent<Rigidbody>().isKinematic = true;
             projectile.GetComponent<Rigidbody>().velocity = new Vector3(40 * kirkMovingRight, 0.0f, 0.0f);
