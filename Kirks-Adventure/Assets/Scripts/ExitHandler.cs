dfb51f17e6a005e184f73fe01ba1fb8d156d11d5
Keegan Dwyer <kmd8132000@gmail.com>
2021-04-18 04:08:45 -0400

    Exit Handlers, Level Completion, End Screen
    Exit Handlers are not present in all necessary scenes. Each Level Complete Screen will correctly lead to the following desired scene. Game is beatable.

--- Kirks-Adventure/Assets/Scripts/ExitHandler.cs
+++ Kirks-Adventure/Assets/Scripts/ExitHandler.cs
@@ -7,51 +7,47 @@ using UnityEngine.SceneManagement;
 
 public class ExitHandler : MonoBehaviour
 {
-    // Fields set in the Unity Inspector pane
     public GameObject player;
     private bool isTripped = false;
-   // public Image youWin;
-    public Image level1Pic;
-    public Image level2Pic;
-
-    private void Start()
-    {
-        //youWin.enabled = false;
-        level1Pic.enabled = false;
-        level2Pic.enabled = false;
-    }
 
     void Update()
     {
-        Scene currentScene = SceneManager.GetActiveScene ();
+        Scene currentScene = SceneManager.GetActiveScene();
         string sceneName = currentScene.name;
         isTripped = false;
 
         if (Math.Abs(player.transform.position.x - this.transform.position.x) < 10 && isTripped == false)
         { 
-            // if ( Input.GetKeyDown(KeyCode.LeftControl) == true )
-            //LEVEL COMPLETE
             print("LEVEL COMPLETE");
-            if(sceneName == "_Level_1_Neighborhood"){
-                level1Pic.enabled = true;
-                if(Input.GetKeyDown(KeyCode.N)){
-                    SceneManager.LoadScene("_Level_2_City");
-                }
+
+            switch(sceneName) {
+                case "_Level_1_Neighborhood":
+                    SceneManager.LoadScene("Level1_Complete");
+                    break;
+
+                case "_Level_2_City":
+                    SceneManager.LoadScene("Level2_Complete");
+                    break;
+
+                case "_Level_3_Country":
+                    isTripped = true;
+                    SceneManager.LoadScene("_WinScreen");
+                    break;
+
+                case "Level1_Complete":
+                    if (Input.GetKeyDown(KeyCode.N))
+                    {
+                        SceneManager.LoadScene("_Level_2_City");
+                    }
+                    break;
+
+                case "Level2_Complete":
+                    if (Input.GetKeyDown(KeyCode.N))
+                    {
+                        SceneManager.LoadScene("_Level_3_Country");
+                    }
+                    break;
             }
-            else if(sceneName == "_Level_2_City"){
-                level2Pic.enabled = true;
-                level1Pic.enabled = false;
-                if(Input.GetKeyDown(KeyCode.N)){
-                    SceneManager.LoadScene("_Level_3_Country");
-                }
-            }
-            else if (sceneName == "_Level_3_Country") {
-                level1Pic.enabled = false;
-                level2Pic.enabled = false;
-                isTripped = true;
-               // youWin.enabled = true;
-            }
-            
         }
     }
 }
